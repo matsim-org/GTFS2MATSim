7416e2d165fbdea7f6ad3f544aa927f05f74cfd7
rakow <rakow@vsp.tu-berlin.de>
2024-12-04 22:12:16 +0100

    option to merge parent and route types

--- src/main/java/org/matsim/contrib/gtfs/GtfsConverter.java
+++ src/main/java/org/matsim/contrib/gtfs/GtfsConverter.java
@@ -2,6 +2,8 @@ package org.matsim.contrib.gtfs;
 
 import com.conveyal.gtfs.GTFSFeed;
 import com.conveyal.gtfs.model.*;
+import it.unimi.dsi.fastutil.ints.IntLinkedOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
@@ -38,16 +40,12 @@ public class GtfsConverter {
     private final MergeGtfsStops mergeStops;
     private final boolean includeMinimalTransferTimes;
     private final String prefix;
-    private LocalDate endDate;
-    private LocalDate startDate;
-
-
     /**
      * Stop that have been mapped to the same facility.
      */
     private final Map<String, Id<TransitStopFacility>> mappedStops = new HashMap<>();
-
-    public enum MergeGtfsStops {doNotMerge, mergeStopsAtSameCoord, mergeToGtfsParentStation}
+    private LocalDate endDate;
+    private LocalDate startDate;
 
     private GtfsConverter(Builder builder) {
         this.feed = Objects.requireNonNull(builder.feed, "Gtfs feed is required, use .setFeed(...)");
@@ -65,15 +63,21 @@ public class GtfsConverter {
         this.includeMinimalTransferTimes = builder.includeMinimalTransferTimes;
         this.prefix = builder.prefix;
         this.endDate = builder.endDate;
-	    if (builder.endDate == null && builder.startDate == null & builder.date != null) {
+        if (builder.endDate == null && builder.startDate == null & builder.date != null) {
             this.startDate = builder.date;
             this.endDate = builder.date;
         }
-	    if (this.endDate != null && this.startDate != null && this.endDate.compareTo(this.startDate) < 0) {
-	        throw new RuntimeException("Start Date " + startDate + " larger than End date " + endDate);
+        if (this.endDate != null && this.startDate != null && this.endDate.compareTo(this.startDate) < 0) {
+            throw new RuntimeException("Start Date " + startDate + " larger than End date " + endDate);
         }
     }
 
+    /**
+     * Creates a new builder for setting converter parameters.
+     */
+    public static Builder newBuilder() {
+        return new Builder();
+    }
 
     public void convert() {
 
@@ -89,7 +93,7 @@ public class GtfsConverter {
         // Put all stops in the Schedule
         this.convertStops();
 
-        if(this.includeMinimalTransferTimes) {
+        if (this.includeMinimalTransferTimes) {
             this.convertTransferTimes();
         }
 
@@ -124,14 +128,14 @@ public class GtfsConverter {
 
         }
         log.info("Latest date mentioned in feed: " + feedEndDate);
-        ts.getAttributes().putAttribute("startDate",startDate.toString());
-        ts.getAttributes().putAttribute("endDate",endDate.toString());
+        ts.getAttributes().putAttribute("startDate", startDate.toString());
+        ts.getAttributes().putAttribute("endDate", endDate.toString());
         LocalDate date = startDate;
         int offsetDays = 0;
         do {
             // Get the used service Id for the chosen weekday and date
             List<String> activeServiceIds = this.getActiveServiceIds(this.feed.services, date);
-            log.info("Active Services: "+ activeServiceIds.size());
+            log.info("Active Services: " + activeServiceIds.size());
 
             // Get the Trips which are active today
             final LocalDate finalDate = date;
@@ -162,12 +166,12 @@ public class GtfsConverter {
                 }
             });
 
-            this.convertTrips(activeTrips,offsetDays);
-            date= date.plusDays(1);
+            this.convertTrips(activeTrips, offsetDays);
+            date = date.plusDays(1);
             offsetDays++;
-        if (activeTrips.isEmpty()) {
-            log.warn("There are no converted trips. You might need to change the date for better results.");
-        }
+            if (activeTrips.isEmpty()) {
+                log.warn("There are no converted trips. You might need to change the date for better results.");
+            }
         } while (!date.isEqual(this.endDate.plusDays(1)));
 
         log.info("Conversion successful");
@@ -182,6 +186,12 @@ public class GtfsConverter {
 
         Map<Coord, Id<TransitStopFacility>> coords = new HashMap<>();
 
+        // Used for mapping stops and route types identifier
+        Map<String, String> routeTypes = null;
+        if (mergeStops == MergeGtfsStops.mergeToParentAndRouteTypes) {
+            routeTypes = buildRouteTypes();
+        }
+
         for (Stop stop : feed.stops.values()) {
 
             if (transformStop != null) {
@@ -192,6 +202,7 @@ public class GtfsConverter {
                 continue;
 
             Coord coord = CoordUtils.round(this.transform.transform(new Coord(stop.stop_lon, stop.stop_lat)));
+            Id<TransitStopFacility> id = getMatsimTransitStopIdFromGtfsStopId(stop.stop_id);
 
             // Already have a stop with same coord
             if (mergeStops.equals(MergeGtfsStops.mergeStopsAtSameCoord) && coords.containsKey(coord)) {
@@ -203,11 +214,21 @@ public class GtfsConverter {
                 continue;
             }
 
-            TransitStopFacility t = this.ts.getFactory().createTransitStopFacility(getMatsimTransitStopIdFromGtfsStopId(stop.stop_id), coord, false);
+            if (routeTypes != null && routeTypes.containsKey(stop.stop_id)) {
+                id = getMatsimTransitStopIdFromGtfsStopId(routeTypes.get(stop.stop_id));
+                mappedStops.put(stop.stop_id, id);
+
+                // Need to check if facility was already created.
+                if (ts.getFacilities().containsKey(id)) {
+                    continue;
+                }
+            }
+
+            TransitStopFacility t = this.ts.getFactory().createTransitStopFacility(id, coord, false);
 
             Pattern pattern = java.util.regex.Pattern.compile("\\p{C}", Pattern.CASE_INSENSITIVE);
 
-            if(pattern.matcher(stop.stop_name).find()){
+            if (pattern.matcher(stop.stop_name).find()) {
                 String cleaned = stop.stop_name.replaceAll("\\p{C}", "");
                 t.setName(cleaned);
             } else
@@ -215,7 +236,7 @@ public class GtfsConverter {
 
             // add only if not yet present
             if (!ts.getFacilities().containsKey(t.getId()))
-               ts.addStopFacility(t);
+                ts.addStopFacility(t);
 
             if (stop.parent_station != null && !stop.parent_station.isEmpty()) {
                 t.setStopAreaId(Id.create(stop.parent_station, TransitStopArea.class));
@@ -225,15 +246,46 @@ public class GtfsConverter {
         }
     }
 
+    /**
+     * Collect all route types that go through a certain stop. Only collected for these with existing parent ids.
+     */
+    private Map<String, String> buildRouteTypes() {
+
+        Map<String, IntSet> routeTypes = new HashMap<>();
+
+        for (Trip trip : feed.trips.values()) {
+            Route route = feed.routes.get(trip.route_id);
+
+            for (StopTime stopTime : feed.getOrderedStopTimesForTrip(trip.trip_id)) {
+                routeTypes.computeIfAbsent(stopTime.stop_id, k -> new IntLinkedOpenHashSet()).add(route.route_type);
+            }
+        }
+
+        Map<String, String> result = new HashMap<>();
+
+        // Build the resulting identifiers
+        for (Stop stop : feed.stops.values()) {
+            if (stop.parent_station == null)
+                continue;
+
+            result.put(stop.stop_id, stop.parent_station + "_" + routeTypes.getOrDefault(stop.stop_id, IntSet.of()).intStream()
+                    .mapToObj(String::valueOf)
+                    .collect(Collectors.joining("_")));
+        }
+
+        return result;
+    }
+
     private Id<TransitStopFacility> getMatsimTransitStopIdFromGtfsStopId(String stopId) {
         return Id.create(prefix + stopId, TransitStopFacility.class);
     }
+
 
     private void convertTransferTimes() {
         for (Transfer transfer : this.feed.transfers.values()) {
             Id<TransitStopFacility> fromStop = findTransitStop(transfer.from_stop_id);
             Id<TransitStopFacility> tostop = findTransitStop(transfer.to_stop_id);
-            if(fromStop != null && tostop != null) {
+            if (fromStop != null && tostop != null) {
                 this.ts.getMinimalTransferTimes().set(fromStop, tostop, transfer.min_transfer_time);
             }
         }
@@ -261,7 +313,7 @@ public class GtfsConverter {
                 if (feed.getOrderedStopTimesForTrip(trip.trip_id) == null || !feed.getOrderedStopTimesForTrip(trip.trip_id).iterator().hasNext()) {
                     log.error("Found a trip with neither frequency nor ordered stop times. Will not add any Matsim TransitRoute/Departure for that trip. GTFS trip_id=" + trip.trip_id);
                     continue;
-                    }
+                }
                 StopTime firstStopTime = feed.getOrderedStopTimesForTrip(trip.trip_id).iterator().next();
                 Double departureTime = Time.parseTime(String.valueOf(firstStopTime.departure_time));
                 List<TransitRouteStop> stops = new ArrayList<>();
@@ -274,18 +326,18 @@ public class GtfsConverter {
                         if (stop == null)
                             continue;
 
-						TransitRouteStop.Builder builder = ts.getFactory().createTransitRouteStopBuilder(stop);
-						if (stopTime.arrival_time != Integer.MIN_VALUE) {
-							double arrivalOffset = Time.parseTime(String.valueOf(stopTime.arrival_time)) - departureTime;
-							builder.arrivalOffset(arrivalOffset);
-						}
-						if (stopTime.departure_time != Integer.MIN_VALUE) {
-							double departureOffset = Time.parseTime(String.valueOf(stopTime.departure_time)) - departureTime;
-							builder.departureOffset(departureOffset);
-						}
-						TransitRouteStop routeStop = builder.build();
-						routeStop.setAwaitDepartureTime(true);
-						stops.add(routeStop);
+                        TransitRouteStop.Builder builder = ts.getFactory().createTransitRouteStopBuilder(stop);
+                        if (stopTime.arrival_time != Integer.MIN_VALUE) {
+                            double arrivalOffset = Time.parseTime(String.valueOf(stopTime.arrival_time)) - departureTime;
+                            builder.arrivalOffset(arrivalOffset);
+                        }
+                        if (stopTime.departure_time != Integer.MIN_VALUE) {
+                            double departureOffset = Time.parseTime(String.valueOf(stopTime.departure_time)) - departureTime;
+                            builder.departureOffset(departureOffset);
+                        }
+                        TransitRouteStop routeStop = builder.build();
+                        routeStop.setAwaitDepartureTime(true);
+                        stops.add(routeStop);
                     }
                 } catch (GTFSFeed.FirstAndLastStopsDoNotHaveTimes firstAndLastStopsDoNotHaveTimes) {
                     throw new RuntimeException(firstAndLastStopsDoNotHaveTimes);
@@ -313,7 +365,7 @@ public class GtfsConverter {
                     for (int time = frequency.start_time; time < frequency.end_time; time += frequency.headway_secs) {
                         TransitLine tl = ts.getTransitLines().get(getReadableTransitLineId(trip));
                         TransitRoute tr = findOrAddTransitRoute(tl, feed.routes.get(trip.route_id), stops);
-                        Departure d = ts.getFactory().createDeparture(Id.create(prefix + trip.trip_id + "." + time+offset, Departure.class), time+offset);
+                        Departure d = ts.getFactory().createDeparture(Id.create(prefix + trip.trip_id + "." + time + offset, Departure.class), time + offset);
                         tr.addDeparture(d);
                         frequencyDepartures++;
                     }
@@ -369,12 +421,7 @@ public class GtfsConverter {
         return Id.create(prefix + asciiShortName + "---" + route.route_id, TransitLine.class);
     }
 
-    /**
-     * Creates a new builder for setting converter parameters.
-     */
-    public static Builder newBuilder() {
-        return new Builder();
-    }
+    public enum MergeGtfsStops {doNotMerge, mergeStopsAtSameCoord, mergeToGtfsParentStation, mergeToParentAndRouteTypes}
 
     public static final class Builder {
 
@@ -386,8 +433,10 @@ public class GtfsConverter {
         private boolean includeMinimalTransferTimes = false;
         private Scenario scenario;
         private Predicate<Trip> includeTrip = (t) -> true;
-        private Consumer<Stop> transformStop = (t) -> {};
-        private Consumer<Route> transformRoute = (t) -> {};
+        private Consumer<Stop> transformStop = (t) -> {
+        };
+        private Consumer<Route> transformRoute = (t) -> {
+        };
         private Predicate<Stop> includeStop = (t) -> true;
         private Predicate<String> includeAgency = (t) -> true;
         private Predicate<Integer> includeRouteType = (t) -> true;
